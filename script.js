868e829210421328d7dec5bbbeaa53649391f076
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-01-13 14:19:06 +0000

    better comments added to script file

--- script.js
+++ script.js
@@ -88,74 +88,86 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+// set the initial value of passwordLength to 0 so that it can be used i nthe getPasswordOptions function
+var passwordLength = 0;
 
+// function to get options once "Generate Password" is clicked
+function getPasswordOptions() {
+passwordLength = prompt('How long would you like your password to be? (between 10 and 64)?');
+speCha = confirm('Would you like to use Special Characters (e.g.$@%&*)?');
+numCha = confirm('Would you like to use Numberic Characters (e.g. 1, 2, 3)?');
+lowCaseCha = confirm('Would you like to use Lowercase Characters?');
+uppCaseCha = confirm('Would you like to use Uppercase Characters?');
+}
 
-var passwordLength = prompt('How long would you like your password to be? (between 8 and 128)?');
-var speCha = confirm('Would you like to use Special Characters (e.g.$@%&*)?');
-var numCha = confirm('Would you like to use Numberic Characters (e.g. 1, 2, 3)?');
-var lowCaseCha = confirm('Would you like to use Lowercase Characters?');
-var uppCaseCha = confirm('Would you like to use Uppercase Characters?');
 
 // Function for getting a random element from an array
-
-
-var randomGeneratedCharacter = [];
+// create a new array for the generated password
+var randomGeneratedPassword = [];
 
 function getRandom() {
-  var firstPasswordOptions = [];
+  // array for the choices made with each itteration of getRandom (to stop all itteration choices going into one main array)
+  var interationchoices = [];
 
+  // if loop for if the user chooses to have special characters. 
   if (speCha === true) {
   var specialCharactersIndex = Math.floor(specialCharacters.length*Math.random());
   var randomSpecialCharacters = specialCharacters[specialCharactersIndex];
-  firstPasswordOptions.push(randomSpecialCharacters);
+  interationchoices.push(randomSpecialCharacters);
   };
 
+  // if loop for if the user chooses to have numeric characters
   if (numCha === true){
   var numericCharactersIndex = Math.floor(numericCharacters.length*Math.random());
-  firstPasswordOptions.push(numericCharactersIndex);
+  interationchoices.push(numericCharactersIndex);
   };
  
+  // if loop for if the user chooses to have lowercase characters
   if (lowCaseCha === true){
   var lowerCasedCharactersIndex = Math.floor(lowerCasedCharacters.length*Math.random());
   var randomLowerCasedCharacters = lowerCasedCharacters[lowerCasedCharactersIndex];
-  firstPasswordOptions.push(randomLowerCasedCharacters);};
+  interationchoices.push(randomLowerCasedCharacters);};
   
+  // if loop for if the user chooses to have uppercase characters
   if (uppCaseCha === true){
   var upperCasedCharactersIndex = Math.floor(upperCasedCharacters.length*Math.random());
   var randomUpperCasedCharacters = upperCasedCharacters[upperCasedCharactersIndex];
-  firstPasswordOptions.push(randomUpperCasedCharacters);};
+  interationchoices.push(randomUpperCasedCharacters);};
 
-  console.log(firstPasswordOptions);
+  // consolelog to check that the above if loops are working
+  // console.log(interationchoices);
 
+  // choosing a random option from the iterationchoises from this loop (loop created in the generatePassword function)
+  var randomGeneratedIndex = Math.floor(interationchoices.length*Math.random());
+  var randomGeneratedChoice = interationchoices[randomGeneratedIndex];
+  randomGeneratedPassword.push(randomGeneratedChoice)
 
-
-  var randomGeneratedIndex = Math.floor(firstPasswordOptions.length*Math.random());
-  var randomGeneratedChoice = firstPasswordOptions[randomGeneratedIndex];
-  randomGeneratedCharacter.push(randomGeneratedChoice)
-
-  console.log(randomGeneratedCharacter);
+  // consolelog to check that the above code is working
+  // console.log(randomGeneratedPassword);
+  
 }
 
-// Function to generate password with user input
-let passwordChoices = [];
+/* Function to generate password using user input (mainly how large the password should be). With each iteration (based on the password length chosen by the user) 
+the generatePassword function is run which chooses a random element chosen from the array created getting random elements from the original arrays */
 function generatePassword() {
   for (let i = 0; i < passwordLength; i++) {
     getRandom();
-    passwordChoices.push(randomGeneratedCharacter);
   }
-  console.log(passwordChoices);
 }
 
-// Get references to the #generate element
+// References to the #generate element
 var generateBtn = document.querySelector('#generate');
 
-// Write password to the #password input
+
+// Function to write out the password in string form then input into the password section on the webpage
 function writePassword() {
-  var password = generatePassword();
+  getPasswordOptions()
+  generatePassword();
   var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
+  var writtenPassword = randomGeneratedPassword.join("");
+  passwordText.value = writtenPassword;
 }
 
-// Add event listener to generate button
+// Event listener to generate button
 generateBtn.addEventListener('click', writePassword);
